fd4cfd475d758a1cfd2744f71e382073ac2aa29b
Your Name <Leonardopilatti14@gmail.com>
2021-01-17 17:34:37 -0300

    scroll suave

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ function initAccordion() {
     }
 }
 initAccordion();
+
+
+/// Animações para scroll suave:
+
+function initScrollSuave() {
+
+    const linksInternos = document.querySelectorAll('a[href^="#"]');
+
+    function scrollToSection(event) {
+        event.preventDefault();
+        const href = event.currentTarget.getAttribute('href');
+        const section = document.querySelector(href)
+
+        section.scrollIntoView({
+            behavior:'smooth',
+            block: 'start',
+        })
+    }
+
+    linksInternos.forEach((link) => {
+        link.addEventListener('click' , scrollToSection);
+    })
+}
+initScrollSuave();
