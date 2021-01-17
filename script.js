3d06289d20ac26fe9ac4a96d65bf5efa70ce7ab9
Your Name <Leonardopilatti14@gmail.com>
2021-01-17 17:22:24 -0300

    layout beneficios e animação na dt e dd

---
+++ script.js
@@ -0,0 +1,20 @@
+function initAccordion() {
+    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const classeAtivo = 'ativo';
+
+    if(accordionList.length){
+
+        accordionList[0].classList.add(classeAtivo);
+        accordionList[0].nextElementSibling.classList.add(classeAtivo)
+
+        function activeAccordion() {
+            this.classList.toggle(classeAtivo);
+            this.nextElementSibling.classList.toggle(classeAtivo);
+        }
+
+        accordionList.forEach((item) => {
+            item.addEventListener('click' , activeAccordion);
+        })
+    }
+}
+initAccordion();
